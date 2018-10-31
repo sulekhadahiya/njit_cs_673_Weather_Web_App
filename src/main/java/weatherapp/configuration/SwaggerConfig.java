b6ac4b154e9e3e936adb86d6347b396daa60c254
Sulekha Dahiya <sulekha@sulekha.rocks>
2018-10-31 03:25:39 -0400

    Added swagger support for Rest API.

---
+++ src/main/java/weatherapp/configuration/SwaggerConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018.
+ */
+
+package weatherapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author sulekha
+ * njit_cs_673_Weather_Web_App, 2018
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+}
